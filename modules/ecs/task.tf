3075a7278e7599c6be8f183ee85747468ae7f817
vilelf <vilelf.dev@gmail.com>
2021-11-29 16:07:09 -0300

    fix wrong path to ecr

--- modules/ecs/task.tf
+++ modules/ecs/task.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_task_definition" "task" {
     {
       "cpu": 128,
       "essential": true,
-      "image": format("%s:%s",aws_ecr_repository.repository_url, var.tag),
+      "image": format("%s:%s", module.ecr.aws_ecr_repository.repository_url, var.tag),
       "memory": 128,
       "name": "api_desafio_devops"
     }
